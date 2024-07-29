42be07ec3d41928b7d3fe154e8b8bccf7679fb7d
Dôn Nguyễn Văn (UPP DEL) <donnv@upp-technology.com>
2024-07-29 16:38:57 +0700

    refactor: refresh token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,54 +1,58 @@
-const jwt = require("jsonwebtoken")
-const { StatusCodes } = require("http-status-codes")
-const { isTokenValid } = require("../utils")
-const { CustomError } = require("./error-handler")
+const jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
+const { isTokenValid } = require('../utils');
+const { CustomError } = require('./error-handler');
 
 const authenticateUser = async (req, res, next) => {
-  const { refreshToken } = req.cookies
-
-  if (!refreshToken) {
-    throw new CustomError(StatusCodes.FORBIDDEN, "No token provided")
-  }
-  isTokenValid(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET)
-
-  const authHeader = req.headers.authorization
-  let accessToken = ""
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomError(StatusCodes.BAD_REQUEST, "Access token is invalid")
-  }
-  accessToken = authHeader.substring(7)
+  const { refreshToken } = req.cookies;
 
   try {
+    if (!refreshToken) {
+      throw new CustomError(StatusCodes.FORBIDDEN, 'No token provided');
+    }
+    isTokenValid(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET);
+
+    const authHeader = req.headers.authorization;
+    let accessToken = '';
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new CustomError(StatusCodes.BAD_REQUEST, 'Access token is invalid');
+    }
+    accessToken = authHeader.substring(7);
+
+    // validate access token
     const decodedToken = jwt.verify(
       accessToken,
       process.env.JWT_ACCESS_TOKEN_SECRET
-    )
-    const { userId, username, email } = decodedToken
+    );
+    const { userId, username, email } = decodedToken;
 
     req.user = {
       userId,
       username,
       email,
-    }
-    next()
+    };
+    next();
   } catch (error) {
-    throw new CustomError(StatusCodes.UNAUTHORIZED, "Access token is invalid")
+    if (error.message?.includes('jwt expired')) {
+      throw new CustomError(StatusCodes.GONE, 'access token expired');
+    }
+    throw new CustomError(StatusCodes.UNAUTHORIZED, 'UNAUTHORIZED');
   }
-}
+};
 
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       throw new CustomError.UnauthorizedError(
-        "Unauthorized to access this route"
-      )
+        'Unauthorized to access this route'
+      );
     }
-    next()
-  }
-}
+    next();
+  };
+};
 
 module.exports = {
   authenticateUser,
   authorizePermissions,
-}
+};
